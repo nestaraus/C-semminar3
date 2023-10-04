6b46be61ca390dc16e175feac047a873cc123d8f
nestaraus <nestaraus@mail.pu>
2023-10-04 21:17:02 +0300

    zadacha 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает произведение чисел от 1 до N
+
+void f(int n, ref int result)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        result*=i;
+    }
+
+}
+
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+f(n,ref result);
+Console.Write(result);

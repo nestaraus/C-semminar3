834e782eeba0c94cf3532d168c30c483d72ba9a7
nestaraus <nestaraus@mail.pu>
2023-10-04 20:56:10 +0300

    zadacha 1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (A) и выдает сумму чисел от 1 до А
+
+void f(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum+=i;
+    }
+    Console.Write(sum);
+}
+
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+f(n);

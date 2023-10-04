3aff8a3d3f46d3407165666fff55ee561dcdeec2
nestaraus <nestaraus@mail.pu>
2023-10-04 21:07:16 +0300

    zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдает количество цифр в этом числе
+
+
+
+
+void f(int n)
+{
+    int kolvo = 0;
+    while (n>0)
+    {
+        kolvo++;
+        n /= 10;
+
+    }
+    Console.Write(kolvo);
+}
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+f(n);

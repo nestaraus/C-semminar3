ae52323e09b24df27a4e65bba9a03f3eb23cd36a
nestaraus <nestaraus@mail.pu>
2023-10-04 20:43:47 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100)
+    Console.Write("Третьей цифры нет");
+else
+{
+    while (n>999)
+    {
+        n/=10;
+    }
+    Console.Write(n%10);
+}
